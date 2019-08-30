7cf580f9f694df15c0ef47e1bb4598f47262f8b9
Jeff Cotton <jeff.cotton@gmail.com>
2019-08-30 14:13:59 -0400

    final build

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,41 +1,65 @@
-var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+const toggles = document.querySelectorAll("a.toggleSlider__toggle")
+const firstBlurb = document.querySelectorAll(".toggleSlider__info-wrap__content-block--first")
 
-const toggles = document.querySelectorAll("a.toggle")
-const firstBlurb = document.querySelectorAll(".first")
+const toogleLinks = document.querySelectorAll('.toggleSlider__hotspots__hotspot');
+const toogleBlocks = document.querySelectorAll('.toggleSlider__info-wrap__content-block');
 
-const toogleLinks = document.querySelectorAll('.hotspot');
-const toogleBlocks = document.querySelectorAll('.content-block');
+const imgWrap = document.querySelector('.toggleSlider__image-wrap');
+const awakeImg = document.querySelector('.toggleSlider__image-wrap--awake img');
 
-const imgWrap = document.querySelector('.image-wrap');
-const awakeImg = document.querySelector('.image-wrap .awake img');
-
-const closeBtn = document.querySelectorAll('.close');
-
-for (const toggle of toggles) {
-    toggle.addEventListener('click', function (event) {
-        event.preventDefault();
-        this.closest(".slide-contain").classList.toggle('toggled');
-        Array.from(toogleBlocks).forEach(block => block.classList.remove('visible'));
-        if (viewportWidth > 940) {
-            Array.from(firstBlurb).forEach(item => item.classList.add('visible'));
-        }
-    })
-}
+const closeBtn = document.querySelectorAll('.toggleSlider__info-wrap__content-block__close');
 
 for (const btn of closeBtn) {
     btn.addEventListener('click', function (event) {
         event.preventDefault();
-        this.closest(".content-block").classList.remove('visible');
+        this.closest(".toggleSlider__info-wrap__content-block").classList.remove('toggleSlider__info-wrap__content-block--visible');
+    })
+}
+
+for (const toggle of toggles) {
+    toggle.addEventListener('click', function (event) {
+        event.preventDefault();
+        this.closest(".toggleSlider").classList.toggle('toggleSlider--toggled');
+        Array.from(toogleBlocks).forEach(block => block.classList.remove('toggleSlider__info-wrap__content-block--visible'));
+        if (window.innerWidth > 940) {
+            Array.from(firstBlurb).forEach(item => item.classList.add('toggleSlider__info-wrap__content-block--visible'));
+        }
+        resizeImg('.toggleSlider__image-wrap--tired img');
+        resizeHotspots('.toggleSlider__hotspots');
+        resizeModal('.toggleSlider__info-wrap__content-block');
     })
 }
 
 Array.from(toogleLinks).forEach(link => {
     link.addEventListener('click', function (event) {
         event.preventDefault();
-        Array.from(toogleBlocks).forEach(item => item.classList.remove('visible'));
+        Array.from(toogleBlocks).forEach(item => item.classList.remove('toggleSlider__info-wrap__content-block--visible'));
         const target = this.getAttribute('href');
-        document.querySelector(target).classList.add('visible');
+        document.querySelector(target).classList.add('toggleSlider__info-wrap__content-block--visible');
     }, false);
+});
+
+window.addEventListener("resize", function () {
+    resizeImg('.toggleSlider__image-wrap--tired img');
+    resizeHotspots('.toggleSlider__hotspots');
+    resizeModal('.toggleSlider__info-wrap__content-block');
+    if (window.innerWidth >= 940) {
+        Array.from(toogleBlocks).forEach(item => item.classList.remove('toggleSlider__info-wrap__content-block--visible'));
+        Array.from(firstBlurb).forEach(item => item.classList.add('toggleSlider__info-wrap__content-block--visible'));
+    } else {
+        Array.from(firstBlurb).forEach(item => item.classList.remove('toggleSlider__info-wrap__content-block--visible'));
+    }
+});
+
+window.addEventListener("load", function () {
+    resizeImg('.toggleSlider__image-wrap--tired img');
+    resizeHotspots('.toggleSlider__hotspots');
+    resizeModal('.toggleSlider__info-wrap__content-block');
+    if (window.innerWidth >= 940) {
+        Array.from(firstBlurb).forEach(item => item.classList.add('toggleSlider__info-wrap__content-block--visible'));
+    } else {
+        Array.from(firstBlurb).forEach(item => item.classList.remove('toggleSlider__info-wrap__content-block--visible'));
+    }
 });
 
 function resizeImg(className) {
@@ -49,20 +73,3 @@ function resizeHotspots(className) {
 function resizeModal(className) {
     document.querySelectorAll(className).forEach(el => el.style.height = imgWrap.clientHeight + 30 + "px");
 }
-
-window.addEventListener("resize", function () {
-    resizeImg('.image-wrap .tired img');
-    resizeHotspots('.hot-spots');
-    resizeModal('.content-block');
-});
-
-window.addEventListener("load", function () {
-    resizeImg('.image-wrap .tired img');
-    resizeHotspots('.hot-spots');
-    resizeModal('.content-block');
-    if (viewportWidth < 940) {
-        Array.from(firstBlurb).forEach(item => item.classList.remove('visible'));
-    } else {
-        Array.from(firstBlurb).forEach(item => item.classList.add('visible'));
-    }
-});
